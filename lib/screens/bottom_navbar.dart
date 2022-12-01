70fafe6a3a11c31048805bf4661ec4e618da9769
kumarsaravana001 <saravanakm46@gmail.com>
2022-12-01 11:53:46 +0530

    new commit

---
+++ lib/screens/bottom_navbar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:ticket_booking_app/screens/search_screen.dart';
+
+import 'home_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 3;
+
+  static final _widgetOptions = [
+    HomeScreen(),
+    SearchScreen(),
+    const Text("Ticket"),
+    const Text("Profile"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Ticket App"),
+      ),
+      body: Center(child: _widgetOptions[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FluentIcons.home_20_regular),
+              activeIcon: Icon(FluentIcons.home_20_filled),
+              label: "home"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentIcons.search_20_regular),
+              activeIcon: Icon(FluentIcons.search_20_filled),
+              label: "search"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentIcons.ticket_horizontal_20_regular),
+              activeIcon: Icon(FluentIcons.ticket_horizontal_20_filled),
+              label: "ticket"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentIcons.person_20_regular),
+              activeIcon: Icon(FluentIcons.person_20_filled),
+              label: "account"),
+        ],
+      ),
+    );
+    //Placeholder();
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          Text('Good Morning'),
+                          Text('Book Ticket'),
+                        ],
+                      ),
+                      const Icon(Icons.airplane_ticket),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
